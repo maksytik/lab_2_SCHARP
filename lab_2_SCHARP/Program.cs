3843a6221e726bfcf666f7517edf3d22308d1ef0
alexsudochakov37@gmail.com <alexsudochakov37@gmail.com>
2020-09-28 21:26:45 +0300

    Добавьте файлы проекта.

---
+++ lab_2_SCHARP/Program.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, m;
+            Console.WriteLine("Введите кол-во четырехугольников:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите кол-во квадратов:");
+
+            m = Convert.ToInt32(Console.ReadLine());
+
+            che[] nmass = new che[n];
+            kvadr[] mmass = new kvadr[m];
+            double[] kords = new double[8];
+
+            Console.WriteLine("координаты вводятся в формате x1 [enter] y1 [enter] x2 [enter] y2 [enter] x3 [enter] y3 [enter] x4 [enter] y4");
+            for (int i = 0; i != n; i++)
+            {
+                Console.WriteLine("Введите координаты точек " + (i + 1) + " четырехугольника");
+                for (int j = 0; j != 8; j++)
+                {
+                    kords[j] = Convert.ToDouble(Console.ReadLine());
+                }
+                nmass[i] = new che(kords);
+
+            }
+
+            for (int i = 0; i != m; i++)
+            {
+                Console.WriteLine("Введите координаты точек " + (i + 1) + " квадрата");
+                for (int j = 0; j != 8; j++)
+                {
+                    kords[j] = Convert.ToDouble(Console.ReadLine());
+                }
+                try
+                {
+                    mmass[i] = new kvadr(kords);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Это не квадрат, попробуйте еще раз");
+                    i--;
+                }
+
+            }
+
+
+            double min = Double.MaxValue, max = Double.MinValue;
+            int minindex = -1, maxindex = -1;
+            for (int i = 0; i != n; i++)
+            {
+                if (nmass[i].perim < min)
+                {
+                    min = nmass[i].perim;
+                    minindex = i;
+                }
+                if (nmass[i].plosh > max)
+                {
+                    max = nmass[i].plosh;
+                    maxindex = i;
+                }
+            }
+            if (n > 0)
+            {
+                Console.WriteLine("Четырехугольник с минимальным периметром под номером " + (minindex + 1) + ", его периметр = " + nmass[minindex].perim);
+                Console.WriteLine("Четырехугольник с максимальной площадью под номером " + (maxindex + 1) + ", его площадь = " + nmass[minindex].plosh);
+            }
+
+            if (m > 0)
+            {
+                for (int i = 0; i != m - 1; i++)
+                {
+                    Console.Write("Квадрату " + (i + 1) + " равны след.:");
+                    for (int j = i + 1; j != m; j++)
+                    {
+                        if (mmass[i].equal(mmass[j]))
+                        {
+                            Console.Write(" " + (j + 1));
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("end");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+/*
+ 2 лаба  12. Создать класс четырехугольник, члены класса – координаты 4-х точек. Предусмотреть в классе
+методы вычисления и вывода сведений о фигуре – длины сторон, диагоналей, периметр, площадь.
+Создать производный класс – квадрат, предусмотреть в классе проверку, является ли фигура
+квадратом. Написать программу, демонстрирующую работу с классом: дано N четырехугольников и M
+квадратов, найти четырехугольники с минимальной и максимальной площадью и номера одинаковых
+квадратов
+ 
+ 
+ */
+/*
+3
+3
+0
+0
+0
+1
+1
+1
+1
+0
+0
+0
+0
+2
+2
+2
+2
+0
+0
+0
+0
+0,5
+0,5
+0,5
+0,5
+0
+0
+0
+0
+1
+1
+1
+1
+0
+0
+0
+0
+2
+2
+2
+2
+0
+0
+0
+0
+1
+1
+1
+1
+0
+
+
+ */
---
+++ lab_2_SCHARP/che.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class che
+    {
+
+        //(x,y) 
+        public readonly double[] kords = { 1, 1,/**/ 1, -1,/**/ -1, -1, /**/ -1, 1 };
+        public readonly double s0, s1, s2, s3, d1, d2;
+        public readonly double perim;
+        public readonly double plosh;
+
+
+        public che(double[] kords)
+        {
+            for (int i = 0; i != 8; i++)
+            {
+                this.kords[i] = kords[i];
+            }
+
+            s0 = storona(0, 1);
+            s1 = storona(1, 2);
+            s2 = storona(2, 3);
+            s3 = storona(3, 0);
+            d1 = storona(0, 2);
+            d2 = storona(1, 3);
+            perim = perimetr();
+            plosh = ploshad();
+        }
+
+        private double storona(int a, int b)
+        {
+            if (a > 3 || a < 0 || b > 3 || b < 0) return -1;
+
+            double x1, y1, x2, y2;
+            x1 = kords[a * 2];
+            y1 = kords[a * 2 + 1];
+            x2 = kords[b * 2];
+            y2 = kords[b * 2 + 1];
+
+            double result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+
+            return result;
+        }
+
+
+        private double perimetr()
+        {
+
+            double result = s0 + s1 + s2 + s3;
+
+
+            return result;
+        }
+
+
+        private double ploshad()
+        {
+
+            double a, b, c, d, e, f, s;
+            a = s0;
+            b = s1;
+            c = s2;
+            d = s3;
+            e = d1;
+            f = d2;
+            s = perim / 2;
+            double result = (s - a) * (s - b) * (s - c) * (s - d) - (1 / 4) * (a * c + b * d + e * f) * (a * c + b * d - e * f);
+            result = Math.Sqrt(result);
+
+            return result;
+        }
+
+        public bool equal(che b)
+        {
+
+            for (int i = 0; i != 8; i++)
+            {
+                if (kords[i] != b.kords[i]) return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+/*
+ 2 лаба  12. Создать класс четырехугольник, члены класса – координаты 4-х точек. Предусмотреть в классе
+методы вычисления и вывода сведений о фигуре – длины сторон, диагоналей, периметр, площадь.
+Создать производный класс – квадрат, предусмотреть в классе проверку, является ли фигура
+квадратом. Написать программу, демонстрирующую работу с классом: дано N четырехугольников и M
+квадратов, найти четырехугольники с минимальной и максимальной площадью и номера одинаковых
+квадратов
+ 
+ 
+ */
+/*
+3
+3
+0
+0
+0
+1
+1
+1
+1
+0
+0
+0
+0
+2
+2
+2
+2
+0
+0
+0
+0
+0,5
+0,5
+0,5
+0,5
+0
+0
+0
+0
+1
+1
+1
+1
+0
+0
+0
+0
+2
+2
+2
+2
+0
+0
+0
+0
+1
+1
+1
+1
+0
+
+
+ */
